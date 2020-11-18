6a32f9889aac67a1d85ae9b1b45b62a65b51f743
Louis Leffler <louisleffler@gmail.com>
2020-11-18 20:21:49 +0000

    Automatically backed up by Learn

--- scratch.rb
+++ scratch.rb
@@ -1,8 +1,5 @@
-def a_method(a,b)
-  puts "I got #{a}"
-  puts "I got #{b}"
-  sum = a + b
-  puts "I got #{sum}"
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
 end
-
-a_method(2,3) 
